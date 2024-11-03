3c96811244f3518f3ca7a9a80de29f974296d6bf
Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>
2024-11-03 06:05:42 +0000

    style: prettify the codes
    Signed-off-by: Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>

--- src/Commands/Trades/stock.ts
+++ src/Commands/Trades/stock.ts
@@ -13,7 +13,7 @@ export const stockTradeCommand = async (client: Client) => {
 		async (ctx) => {
 			const useChart = ctx.flags.includes('chart') || ctx.flags.includes('use-chart');
 			const code = ctx.args.at(0);
-            const useLong = ctx.flags.includes('long');
+			const useLong = ctx.flags.includes('long');
 
 			if (!code) {
 				return;
@@ -34,9 +34,9 @@ export const stockTradeCommand = async (client: Client) => {
 						`saham_${code.toUpperCase()}`,
 						60,
 						JSON.stringify({
-                            ...data,
-                            _cache: 1,
-                        }),
+							...data,
+							_cache: 1,
+						}),
 					);
 				}
 
@@ -60,126 +60,143 @@ export const stockTradeCommand = async (client: Client) => {
 					(await redisClient.get(`saham_plot_${code.toUpperCase()}`)) ?? '',
 				) ?? (await getStockPlot(code));
 
-            if (!data?.Code) {
-                await ctx.reply(`Stock *${code}* doesnt exist`);
+			if (!data?.Code) {
+				await ctx.reply(`Stock *${code}* doesnt exist`);
 				return;
-            }
+			}
 
 			if (!Reflect.has(data, '_cache')) {
-				await redisClient.setex(`saham_${code.toUpperCase()}`, 60, JSON.stringify({
-                    ...data,
-                    _cache: 1,
-                }));
+				await redisClient.setex(
+					`saham_${code.toUpperCase()}`,
+					60,
+					JSON.stringify({
+						...data,
+						_cache: 1,
+					}),
+				);
 			}
 
 			const canvas = new ChartJSNodeCanvas({
 				height: useLong ? 1000 : 500,
 				width: useLong ? 5000 : 2000,
-                backgroundColour: 'white',
+				backgroundColour: 'white',
 			});
 
 			// Data
-            data.Returns = data.Returns.slice(useLong ? 0 : -120);
-			const labels = data.Returns.map((ret) => ret.Date).map(x => dayjs(x).format('DD/MM/YYYY'));
+			data.Returns = data.Returns.slice(useLong ? 0 : -120);
+			const labels = data.Returns.map((ret) => ret.Date).map((x) =>
+				dayjs(x).format('DD/MM/YYYY'),
+			);
 			const lows = data.Returns.map((ret) => ret.Low);
 			const closes = data.Returns.map((ret) => ret.Close);
 			const highs = data.Returns.map((ret) => ret.High);
 			const opens = data.Returns.map((ret) => ret.Open);
-			const years = [...new Set(data.Returns.map(ret => dayjs(ret.Date).format('YYYY')))];
+			const years = [...new Set(data.Returns.map((ret) => dayjs(ret.Date).format('YYYY')))];
 
-            const averageData = years.map(year => {
-                const datas = data.Returns.filter(ret => dayjs(ret.Date).format('YYYY') === year).map(ret => ret.Close);
-				return `- ${year} (${(datas.reduce((a, b, _, d) => a + (b/d.length), 0)).toFixed(2)})`;
-            });
+			const averageData = years.map((year) => {
+				const datas = data.Returns.filter(
+					(ret) => dayjs(ret.Date).format('YYYY') === year,
+				).map((ret) => ret.Close);
+				return `- ${year} (${datas.reduce((a, b, _, d) => a + b / d.length, 0).toFixed(2)})`;
+			});
 
-            const m = await ctx.reply(`Generating chart for *${data.Name}* (${data.Code})\n${labels.length} data`);
-			const buffer = await canvas.renderToBuffer({
-				type: 'line',
-				data: {
-					labels,
-					datasets: [
-						{
-							label: 'Low',
-							data: lows,
-							borderColor: 'red',
-							fill: false,
-							showLine: true,
-						},
-						{
-							label: 'High',
-							data: highs,
-							borderColor: 'green',
-							fill: false,
-							showLine: true,
-						},
-						{
-							label: 'Open',
-							data: opens,
-							borderColor: 'blue',
-							fill: false,
-							showLine: true,
-						},
-						{
-							label: 'Close',
-							data: closes,
-							borderColor: 'gray',
-							fill: false,
-							showLine: true,
-						},
-					],
-				},
-				options: {
-                    plugins: {
-                        title: {
-                            text: `${data.Name} (${data.Code}) ${data.Returns.length} Days Price Chart`,
-                            display: true,
-                            align: 'center',
-                            position: 'top',
-                            color: 'black',
-                        },
-						legend: {
-						}
-                    },
-					responsive: false,
-					scales: {
-						x: {
-							title: {
-								display: true,
-								text: 'Date',
+			const m = await ctx.reply(
+				`Generating chart for *${data.Name}* (${data.Code})\n${labels.length} data`,
+			);
+			const buffer = await canvas.renderToBuffer(
+				{
+					type: 'line',
+					data: {
+						labels,
+						datasets: [
+							{
+								label: 'Low',
+								data: lows,
+								borderColor: 'red',
+								fill: false,
+								showLine: true,
 							},
-                            ticks: {
-                                display: true,
-                                autoSkip: false,
-                            },
-                            pointLabels: {
-                                display: true,
-                            },
-						},
-						y: {
-							title: {
-								display: true,
-								text: 'Price',
+							{
+								label: 'High',
+								data: highs,
+								borderColor: 'green',
+								fill: false,
+								showLine: true,
 							},
-                            min: data.Returns.sort((a, b) => a.Open - b.Open)[0].Open < 600 ? 0 : data.Returns.sort((a, b) => a.Open - b.Open)[0].Open - 500,
-                            beginAtZero: data.Returns.sort((a, b) => a.Open - b.Open)[0].Open <= 500,
-                            ticks: {
-                                display: true,
-                                align: 'center',
-                                autoSkip: false,
-                            },
-                            pointLabels: {
-                                display: true,
-                            },
+							{
+								label: 'Open',
+								data: opens,
+								borderColor: 'blue',
+								fill: false,
+								showLine: true,
+							},
+							{
+								label: 'Close',
+								data: closes,
+								borderColor: 'gray',
+								fill: false,
+								showLine: true,
+							},
+						],
+					},
+					options: {
+						plugins: {
+							title: {
+								text: `${data.Name} (${data.Code}) ${data.Returns.length} Days Price Chart`,
+								display: true,
+								align: 'center',
+								position: 'top',
+								color: 'black',
+							},
+							legend: {},
+						},
+						responsive: false,
+						scales: {
+							x: {
+								title: {
+									display: true,
+									text: 'Date',
+								},
+								ticks: {
+									display: true,
+									autoSkip: false,
+								},
+								pointLabels: {
+									display: true,
+								},
+							},
+							y: {
+								title: {
+									display: true,
+									text: 'Price',
+								},
+								min:
+									data.Returns.sort((a, b) => a.Open - b.Open)[0].Open < 600
+										? 0
+										: data.Returns.sort((a, b) => a.Open - b.Open)[0].Open -
+											500,
+								beginAtZero:
+									data.Returns.sort((a, b) => a.Open - b.Open)[0].Open <= 500,
+								ticks: {
+									display: true,
+									align: 'center',
+									autoSkip: false,
+								},
+								pointLabels: {
+									display: true,
+								},
+							},
 						},
 					},
 				},
-			}, 'image/png');
+				'image/png',
+			);
 
-            await m?.delete();
+			await m?.delete();
 			const lastData = data.Returns.at(-1);
 			const text = `Saham Chart *${data.Name}* (${data.Code})\nLast open: ${lastData?.Open}\nLast high: ${lastData?.High}\nLast low: ${lastData?.Low}\nLast vol: ${lastData?.Volume}\nLast Date: ${lastData?.Date}\nAverage:\n	${averageData.join('\n	')}`;
-			
-            await ctx.replyWithPhoto(buffer, text);
+
+			await ctx.replyWithPhoto(buffer, text);
 		},
 		{
 			aliases: ['idx-stock', 'stock-trade', 'strade'],
